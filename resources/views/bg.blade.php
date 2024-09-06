0538c8d2e981c8559c81b798028efcca824ed6e5
Vova <tomaslipton@gmail.com>
2024-09-06 15:42:05 +0200

    init

---
+++ resources/views/bg.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Przygotowania do Karty Polaka</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <style>
+        body {
+            background: linear-gradient(to bottom, #e9e8e7 50%, red 50%);
+            height: 100vh;
+            margin: 0;
+            backdrop-filter: blur(30px);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .container {
+            width: 100%;
+            max-width: 1024px;
+            height: 100%;
+            max-height: 720px;
+            background: #fdfdfd;
+            border-radius: 24px;
+        }
+    </style>
+</head>
+<body >
+<div class="container shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)]">
+<h1>Witam</h1>
+</div>
+</body>
+</html>
