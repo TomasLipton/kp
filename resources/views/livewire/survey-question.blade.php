187a730eefb1b0cc486b281b454cc219e8b9da66
Vova <tomaslipton@gmail.com>
2024-09-24 21:06:15 +0200

    ui

--- resources/views/livewire/survey-question.blade.php
+++ resources/views/livewire/survey-question.blade.php
@@ -5,9 +5,9 @@
 
 @script
 <script>
-    Livewire.hook('component.init', ({ component, cleanup }) => {
+    Livewire.hook('component.init', ({component, cleanup}) => {
         document.addEventListener('keydown', function (event) {
-console.log(document.getElementById('submit').length)
+            console.log(document.getElementById('submit').length)
             if (event.key === 'Enter' && document.getElementById('wrap').dataset.isAnswered === 'true') {
                 $wire.nextQuestion();
             }
@@ -79,37 +79,36 @@ console.log(document.getElementById('submit').length)
             </div>
             <div class="answers" style="">
                 @if($question->picture)
-                <div>
-                    <img src="{{url('storage/' . $question->picture)}}" style=" max-width: 300px; border-radius: 8px;" alt="">
-                </div>
+                    <div>
+                        <img src="{{url('storage/' . $question->picture)}}" style=" max-width: 300px; border-radius: 8px;" alt="">
+                    </div>
                 @endif
                 <div style=" width: 100%; ">
                     @if($question->question_type === 'single_text')
-                    <ol>
-                        @foreach($question->answers as $answer)
-                            <li wire:key="{{ $answer->id }}" wire:click.debounce.1000ms="submitAnswer('{{$answer->id}}')" @if($chosenAnswer && $answer->id == $chosenAnswer->id) style="text-decoration: underline" @endif >  {{$answer->text}}</li>
-                        @endforeach
-                    </ol>
+                        <ol>
+                            @foreach($question->answers as $answer)
+                                <li wire:key="{{ $answer->id }}" wire:click.debounce.1000ms="submitAnswer('{{$answer->id}}')" @if($chosenAnswer && $answer->id == $chosenAnswer->id) style="text-decoration: underline" @endif >  {{$answer->text}}</li>
+                            @endforeach
+                        </ol>
                     @endif
 
                     @if($question->question_type === 'year')
-                      <div class="answer-year-container"  x-data="{ inputValue: '' }">
-                          <input autofocus class="year-answer"   x-model="inputValue"
-                                 wire:keydown.enter="submitYear($event.target.value)"
-                                 x-on:input="inputValue = inputValue.slice(0, 4).replace(/\D/g, '')"
-                                 type="text"
-                                 pattern="[0-9]*"
-                                 inputmode="numeric"
-                                 @if($chosenAnswer) readonly @endif
-                                 maxlength="4" />
-{{--                          <div class="button-container">--}}
-                              <button class="submit-button">
-                                  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="100" viewBox="0 0 40 40">
-                                      <path fill="#bae0bd" d="M1.707 22.199L4.486 19.42 13.362 28.297 35.514 6.145 38.293 8.924 13.362 33.855z"></path><path fill="#5e9c76" d="M35.514,6.852l2.072,2.072L13.363,33.148L2.414,22.199l2.072-2.072l8.169,8.169l0.707,0.707 l0.707-0.707L35.514,6.852 M35.514,5.438L13.363,27.59l-8.876-8.876L1,22.199l12.363,12.363L39,8.924L35.514,5.438L35.514,5.438z"></path>
-                                  </svg>
-                              </button>
-{{--                          </div>--}}
-                      </div>
+                        <div class="answer-year-container" x-data="{ inputValue: '' }">
+                            <input autofocus class="year-answer" x-model="inputValue"
+                                   wire:keydown.enter="submitYear($event.target.value)"
+                                   x-on:input="inputValue = inputValue.slice(0, 4).replace(/\D/g, '')"
+                                   type="text"
+                                   pattern="[0-9]*"
+                                   inputmode="numeric"
+                                   @if($chosenAnswer) readonly @endif
+                                   maxlength="4"/>
+                            <button class="submit-button" @click="$wire.submitYear(inputValue)">
+                                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="100" viewBox="0 0 40 40">
+                                    <path fill="#bae0bd" d="M1.707 22.199L4.486 19.42 13.362 28.297 35.514 6.145 38.293 8.924 13.362 33.855z"></path>
+                                    <path fill="#5e9c76" d="M35.514,6.852l2.072,2.072L13.363,33.148L2.414,22.199l2.072-2.072l8.169,8.169l0.707,0.707 l0.707-0.707L35.514,6.852 M35.514,5.438L13.363,27.59l-8.876-8.876L1,22.199l12.363,12.363L39,8.924L35.514,5.438L35.514,5.438z"></path>
+                                </svg>
+                            </button>
+                        </div>
                     @endif
                 </div>
             </div>
