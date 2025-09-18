b19f293532ca4378ca6e8423ab250bc3f3511b9d
Vova <tomaslipton@gmail.com>
2025-09-18 15:27:49 +0400

    ui

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/css/quiz.scss',
+                'resources/css/start-survey.scss',
                 'resources/js/app.js',
             ],
             refresh: true,
