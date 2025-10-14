c60d1e30d61dcf4f4d0475577ac8a32cfd1c2267
Vova <tomaslipton@gmail.com>
2025-10-14 23:08:50 +0200

    js server

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,9 @@ Route::group([
     //        ->name('ai');
     //
     //    Volt::route('ai-realtime-configure', 'ai-realtime-configure')->name('ai-quiz');
-    Volt::route('history', 'ai-quiz-history')->name('ai-quiz-history');
+    Volt::route('history', 'ai-quiz-history')
+        ->middleware(['auth'])
+        ->name('ai-quiz-history');
     Volt::route('ai-voice-quiz', 'ai-sync-configure')->name('ai-sync-configure');
     Volt::route('ai-voice-quiz/{quiz}', 'ai-sync-play')->name('voice-quiz');
     Volt::route('ai-voice-quiz/{quiz}/summary', 'ai-quiz-summary')->name('ai-quiz-summary');
