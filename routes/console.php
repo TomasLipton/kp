6180ba32fe47a3aeabf564f2ff47e068e1990ae7
Vova <tomaslipton@gmail.com>
2025-10-15 16:04:13 +0200

    clear console.php

--- routes/console.php
+++ routes/console.php
@@ -2,21 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use OpenAI\Laravel\Facades\OpenAI;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
-
-Artisan::command('d', function () {
-    $response = OpenAI::realtime()->token([
-//    'session' => [
-        'instructions' => 'You are a teacher'
-//    ]
-    ]);
-dd($response->clientSecret->expiresAt);
-dd($response->clientSecret->value);
-
-})->purpose('Display an inspiring quote')->hourly();
-
-Artisan::command('a', function () {})->purpose('Display an inspiring quote')->hourly();
