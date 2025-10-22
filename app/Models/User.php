815dc4e6dbe2c976d845539de7e855107d083ebc
Vova <tomaslipton@gmail.com>
2025-10-22 15:21:43 +0200

    stripe

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
 
 class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
+    use Billable;
 
     /**
      * The attributes that are mass assignable.
--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,29 @@ Route::group([
     'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath'],
 ], function () {
 
+//    Route::get('/subscribe', function (Request $request) {
+//        $user = \Illuminate\Support\Facades\Auth::user();
+//
+//        return $user->newSubscription('default', 'price_1SAtFwPkPU7QethbODxZxYbb')
+//            ->checkout([
+//                'success_url' => route('dashboard') . '?session_id={CHECKOUT_SESSION_ID}',
+//                'cancel_url' => route('dashboard'),
+//            ]);
+//    });
+//
+//    Route::get('/billing-portal', function (Request $request) {
+//        $user = \Illuminate\Support\Facades\Auth::user();
+//
+//        return $user->redirectToBillingPortal();
+//    });
+
+
+
     Livewire::setUpdateRoute(function ($handle) {
         return Route::post('/livewire/update', $handle);
     });
 
-    Route::get('/', \App\Livewire\Main::class)->name('dashboard');
+    Route::get('/', \App\Livewire\Main::class)->name('home');
 
     Volt::route('topics', 'topics')->name('topics');
 
